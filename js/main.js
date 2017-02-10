787009ff71b27c7f6cc253b9a5fc675b3e161f45
Yash Trivedi <yasht93@live.com>
2017-02-10 14:02:29 +0530

    Intermediate Commit
    Added Bounce Animation on opening InfoWindow

--- js/main.js
+++ js/main.js
@@ -15,6 +15,13 @@ var locations = [
                 }
             },
             {
+                title: 'Pashan Lake',
+                location: {
+                    lat: 18.534397,
+                    lng: 73.785292
+                }
+            },
+            {
                 title: 'Lavale, Pune',
                 location: {
                     lat: 18.516655,
@@ -79,9 +86,9 @@ locInfo = new google.maps.InfoWindow();
             id:i
         });
         console.log(locations[i].title);
-        console.log('New InfoWindow Created');
         locations[i].marker.addListener('click', function() {
             console.log('InfoWindow Loop entered');
+            toggleBounce(this);
             populateInfoWindow(this, locInfo);
         });
         wikiLink(locations[i].marker);
@@ -103,7 +110,7 @@ function wikiLink(marker) {
                 alert("No Wikipedia results found!");
             }
             marker.url = response[3];
-            console.log(marker.url);
+            //console.log(marker.url);
             clearTimeout(wikiReqTimeout);
         }
     });
@@ -113,16 +120,28 @@ function wikiLink(marker) {
 function populateInfoWindow(marker,locInfo) {
     locInfo.marker = marker;
     if (marker.url != undefined) {
-        var wikiString = '<div><h2>' + marker.title + '</h2><br><hr><h4><a href = "' + marker.url + '">' + 'Wikipedia Link' + '</a></h4>';
+        var heading = '<div><h2>' + marker.title + '</h2></div>';
+        var wikiString = '<hr><h4><a href = "' + marker.url + '">' + 'Wikipedia Link' + '</a></h4>';
         var streetViewString = '<img src ="https://maps.googleapis.com/maps/api/streetview?size=300x100&location=' + marker.title + '&key=AIzaSyAxD5mgFLhdkpdMY75kZGPpe7-luaa3Qso">';
-        var infoString = wikiString + '<div>' + streetViewString + '</div>';
+        var infoString = heading + '<div>' + wikiString + '</div><div>' + streetViewString + '</div>';
         locInfo.setContent(infoString);
+        console.log("InfoWindow code: " + infoString);
     }
     else {
         locInfo.setContent("No info available!");
     }
     locInfo.open(map, marker);
     locInfo.addListener('closeclick', function() {
-        locInfo.marker = null;
+        locInfo.setAnimation(null);     // in case the marker has not stopped animating, do so on closing it
+        locInfo.marker = null;      //set null as the info in infoWindow
+
     });
+}
+
+//function for toggling bounce animation on selected infoWindow:
+function toggleBounce(marker) {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+    setTimeout(function() {
+        marker.setAnimation(google.maps.Animation.null);
+    }, 5000);
 }
